385571731eb36ca4cb73fd40bcee2544e2b39f65
TKaburu <traceymwendwa@gmail.com>
2024-01-04 12:46:37 -0800

    Feat: Sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: The pointer to the node to find the sibling
+ *
+ * Return: siblin or NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	/* Need to know if its a left child or right */
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+		return (node->parent->left);
+	else
+		return (NULL);
+}
