eb4f294e4d45ab07701681ba86c07e8c068bdf1a
TKaburu <traceymwendwa@gmail.com>
2024-01-04 02:17:49 -0800

    fix: Betty style indentation

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,11 +9,11 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-        if (tree == NULL || func == NULL)
-                return;
+	if (tree == NULL || func == NULL)
+		return;
 
-        /*PostOrder = left->right->root*/
-        binary_tree_postorder(tree->left, func);
-        binary_tree_postorder(tree->right, func);
-        func(tree->n);
+	/*PostOrder = left->right->root*/
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
